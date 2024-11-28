164446f9ea5fad130f416a101cc41cf1001e215e
Singh <singhg161@student.douglascollege.ca>
2024-11-28 13:33:34 -0800

    jwt working fine

--- admin/admin.js
+++ admin/admin.js
@@ -1,9 +1,46 @@
 import express from "express";
-
+import jwt from "jsonwebtoken";
 
 export const adminRouter = express.Router();
 
+function verifyJsonToken(req, res, next) {
+  let rawToken = req.headers.authorization;
+  let token = rawToken.split(" ")[1];
+  try {
+    if (jwt.verify(token, process.env.SECRET_KEY)) {
+      next();
+    }
+  } catch (ex) {
+    res.status(401).json({ error: "bad request" });
+  }
+}
 
-adminRouter.get("/",(req,res)=>{
-    res.status(200).json("this is working fine")
-})
+adminRouter.post("/login", (req, res) => {
+  let { userName, password } = req.body;
+  if (process.env.ADMIN_USERNAME === userName) {
+    if (process.env.ADMIN_PASSWORD === password) {
+      let token = jwt.sign(
+        {
+          userName: process.env.ADMIN_USERNAME,
+          password: process.env.ADMIN_PASSWORD,
+        },
+        process.env.SECRET_KEY
+      );
+      res.status(200).json({ token: token });
+    } else {
+      res.status(401).json({ error: "invalide password" });
+    }
+  } else {
+    res.status(401).json({ error: "invalid user name" });
+  }
+});
+
+adminRouter.get(
+  "/",
+
+  verifyJsonToken,
+
+  (req, res) => {
+    res.status(200).json("this is working fine");
+  }
+);
--- index.js
+++ index.js
@@ -1,18 +1,23 @@
 import express from "express";
 import cors from "cors";
 import bodyParser from "body-parser";
+import dotenv from "dotenv"
+
 import {db} from "./database/connect.js";
 import {collRouter} from "./collections/collections.js"
 import {jwlRouter} from "./jewellry/jewellry.js"
 import {adminRouter} from "./admin/admin.js"
 
-const admin = {
-    userName:"admin",
-    userPassword:"password"
-}
 
-
+dotenv.config();
 const app = express();
+
+
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded());
+
 
 app.use("/api/admin",adminRouter);
 app.use("/api/jewellries",jwlRouter);
@@ -23,7 +28,7 @@ app.use("/api/collections",collRouter);
 
 
 
-app.listen(3000,()=>{
+app.listen(process.env.PORT,()=>{
     console.log("http://localhost:3000/api");
 })
 
